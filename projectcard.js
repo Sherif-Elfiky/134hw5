67a12e9dcbe825caf4c8fb8f5aa7728b9dc5e583
Sherif-Elfiky <114533563+Sherif-Elfiky@users.noreply.github.com>
2025-03-15 20:03:15 -0700

    added food.png and modified project card to take pictures

--- projectcard.js
+++ projectcard.js
@@ -11,53 +11,42 @@ class projectcard extends HTMLElement {
                     justify-content: center;
                     flex-direction: column;
                     border: 2px solid;
-                    border-color: --mixed-border-color;
+                    border-color: var(--mixed-border-color);
                     margin: 3em;
                     transition: transform 0.3s ease-in-out;
-                 
-                    
-                    
-                    
-                    
                 }
 
                 :host(:hover) {
                     transform: scale(1.05);
                 }
 
-                
-
-              
+                picture {
+                    display: flex;
+                    justify-content: center;
+                }
 
                 p {
                     margin: 3rem;
-                  
                     color: var(--text-color, #330066);
                 }
 
                 img {
                     margin: auto;
-                    width: 90px;
-                    height: 50px;
-
+                    width: 130px;
+                    height: 75px;
                 }
 
                 a {
-                   
                     background-color: var(--primary-color, #2E0854);
                     color: white;
                     border: 1px solid var(--secondary-color, #2E0854);
                     margin: .5em;
-                  
-                   
                 }
 
                 a:hover {
                     background-color: white;
                     color: var(--secondary-color);
                     border-color: var(--mixed-border-color);
-                   
-
                 }
 
                 @media (max-width: 600px) {
@@ -74,12 +63,12 @@ class projectcard extends HTMLElement {
             </style>
 
             <h2></h2>
-            <img />
+            <picture>
+                <!-- The <img> will be dynamically updated -->
+            </picture>
             <p></p>
             <a target="_blank">Learn More</a>
         `;
-
-
     }
 
     connectedCallback() {
@@ -87,7 +76,7 @@ class projectcard extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ["title", "image", "description", "link"];
+        return ["title", "description", "link", "picture"];
     }
 
     attributeChangedCallback() {
@@ -95,12 +84,26 @@ class projectcard extends HTMLElement {
     }
 
     render() {
+        // Set the project title
         this.shadowRoot.querySelector("h2").textContent = this.getAttribute("title") || "Project Name";
-        this.shadowRoot.querySelector("img").src = this.getAttribute("image") || "placeholder.jpg";
-        this.shadowRoot.querySelector("img").alt = this.getAttribute("title") || "Project image";
+    
+        // Get the image source (expects PNG)
+        const imageSrc = this.getAttribute("picture") || "placeholder.png"; // default to placeholder if no image is provided
+        const pictureElement = this.shadowRoot.querySelector("picture");
+    
+        // Create the <img> element for the picture tag to support PNG images
+        pictureElement.innerHTML = `
+            <img src="${imageSrc}" alt="${this.getAttribute('title')}">
+        `;
+    
+        // Set the project description
         this.shadowRoot.querySelector("p").textContent = this.getAttribute("description") || "No description available.";
+    
+        // Set the link URL
         this.shadowRoot.querySelector("a").href = this.getAttribute("link") || "#";
     }
+    
+
 }
 
 customElements.define("project-card", projectcard);
